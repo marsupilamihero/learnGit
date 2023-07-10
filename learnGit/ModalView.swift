ffadd3af36fd207cd0ee06f5847afe3b1380bb9c
Rico Oktananda <ricokwee13@gmail.com>
2023-07-10 21:04:21 +0700

    Second Commit

---
+++ learnGit/ModalView.swift
@@ -0,0 +1,20 @@
+//
+//  ModalVieww.swift
+//  learnGit
+//
+//  Created by Rico Oktananda on 10/07/23.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text("Modal View")
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalView()
+    }
+}
