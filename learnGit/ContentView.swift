7a4e2ccc72b488f351d632f2701b2c09d731dd07
Rico Oktananda <ricokwee13@gmail.com>
2023-07-10 20:55:40 +0700

    Initial Commit

---
+++ learnGit/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  learnGit
+//
+//  Created by Rico Oktananda on 10/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isPresented = false
+    var body: some View {
+        NavigationView {
+            Button("Show Modal") {
+                self.isPresented = true
+            }.sheet(isPresented: $isPresented) {
+                Text("This is the modal screen")
+            }
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
---
+++ learnGit/learnGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnGitApp.swift
+//  learnGit
+//
+//  Created by Rico Oktananda on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct learnGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
